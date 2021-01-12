b75341d96ce4eb50cecec0aa38f63905dd1ced06
CSWILIK <cswilik@gmail.com>
2021-01-12 08:55:02 -0500

    with my notes

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,9 +1,18 @@
 import React, { useState } from "react";
 
 function Counter() {
-  const [count, setCount] = useState(0);
+  // useState will return an array of two things:
+  // count(current value) + setCount(a setter function so we can update count)
+  // below could be written as follows:
+  //    const countState = useState(0);
+//      const count = countState[0];
+//      const setCount = countState[1];
+const [count, setCount] = useState(0);
+  
 
   function increment() {
+    // setting state is asynchronous!!!
+    // rule of thumb, should use callback when updating state
     setCount(count + 1);
   }
 
